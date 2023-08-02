4de5fe08d9276d33d19b9dc3cfe0b2ee53fc7cf2
Josoe Santos <jssantosqueiroz@gmail.com>
2023-08-01 21:02:01 -0300

    feat: :sparkles: cofigure tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex justify-center items-center flex-col min-h-screen">
       <Head>
         <title>Josoe Santos</title>
         <meta name="description" content="Web developer an technology lover" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="flex flex-col items-center gap-3">
+        <h1 className="text-6xl font-bold">
           Welcome to what will be my own site.
         </h1>
-        <dl>
-          <dt className={styles.descriptionTitle}>Github: </dt>
-          <dd><a href="https://github.com/JosoeSantos">@JosoeSantos</a></dd>
-          <dt className={styles.descriptionTitle}>Discord: </dt>
-          <dd>JosOe#9705</dd>
+        <dl className='max-w-max'>
+          <dt className="text-gray-500">Github: </dt>
+          <dd><a className='text-indigo-600 hover:text-indigo-500 visited:text-red-500 pl-3' href="https://github.com/JosoeSantos">@JosoeSantos</a></dd>
+          <dt className="text-gray-500 ">Discord: </dt>
+          <dd className='pl-3'>JosOe#9705</dd>
         </dl>
       </main>
     </div>
---
+++ pages/lab/seo-meta.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const SeoMetaPage: NextPage = () => {
+  return <h1>Testar meta tags para SEO</h1>
+};
+
+export default SeoMetaPage;
