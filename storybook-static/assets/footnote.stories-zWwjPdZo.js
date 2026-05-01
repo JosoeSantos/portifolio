1f02a65b31aec2ac0b617743a0bbb11fdf026418
Josoe <jssantosqueiroz@gmail.com>
2026-05-01 18:02:29 -0300

    fix: :fire: delete storybook files

--- storybook-static/assets/footnote.stories-zWwjPdZo.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-BEldbCjX.js";import{u as t}from"./iframe-D6d4QXiA.js";function n({n:e,children:t}){return(0,r.jsxs)(`span`,{className:`group`,children:[(0,r.jsx)(`sup`,{className:`text-ochre font-mono text-[0.75rem]`,children:e}),(0,r.jsx)(`span`,{children:t})]})}var r,i=e((()=>{r=t(),n.__docgenInfo={description:``,methods:[],displayName:`Footnote`,props:{n:{required:!0,tsType:{name:`number`},description:``},children:{required:!0,tsType:{name:`ReactReactNode`,raw:`React.ReactNode`},description:``}}}})),a,o,s,c;e((()=>{a=t(),i(),o={title:`Components/Footnote`,component:n},s={render:()=>(0,a.jsxs)(`p`,{style:{fontFamily:`serif`,lineHeight:1.6},children:[`most things are more complicated than they appear`,(0,a.jsx)(n,{n:1,children:`this includes footnotes`}),`.`]})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  render: () => <p style={{
-    fontFamily: "serif",
-    lineHeight: 1.6
-  }}>
-      most things are more complicated than they appear
-      <Footnote n={1}>this includes footnotes</Footnote>.
-    </p>
-}`,...s.parameters?.docs?.source}}},c=[`Default`]}))();export{s as Default,c as __namedExportsOrder,o as default};
